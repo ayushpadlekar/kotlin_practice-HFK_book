6acaa5e571f743aae85857817a347b26ce63dd89
Ayush Padlekar <110286909+ayushpadlekar@users.noreply.github.com>
2025-03-04 18:43:17 +0530

    Create 76_page-YabbaDabbaDo_Exercise.kt

---
+++ 01_Chapter-Getting_Started/76_page-YabbaDabbaDo_Exercise.kt
@@ -0,0 +1,14 @@
+fun main() {
+	
+    // HFK page 76-77 - Code Magnets exercise - YabbaDabbaDo
+
+    var x = 1
+
+    while (x < 3) {
+        print(if(x == 1) "Yab" else "Dab")
+        print("ba")
+        x = x + 1
+    }
+    if (x == 3) println("Do")
+
+}
