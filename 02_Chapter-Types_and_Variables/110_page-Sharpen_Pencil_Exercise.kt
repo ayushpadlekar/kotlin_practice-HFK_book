8a82c7ad86bae4c43af69547c6f22279d555e5f3
Ayush Padlekar <110286909+ayushpadlekar@users.noreply.github.com>
2025-03-05 23:54:17 +0530

    Create 110_page-Sharpen_Pencil_Exercise.kt

---
+++ 02_Chapter-Types_and_Variables/110_page-Sharpen_Pencil_Exercise.kt
@@ -0,0 +1,40 @@
+fun main() {
+    // HFK C2 page 110 - Sharpen your Pencil exercise
+
+
+    var x: Int = 65.2
+
+    var isPunk = true
+
+    println(isPunk)
+
+    var message = 'Hello'
+    println(message)
+
+    var y = 7
+
+    var z: Int = y
+
+    y = y + 50
+
+    var s: Short
+
+    var bigNum: Long = y.toLong()
+
+    var b: Byte
+
+    var smallNum = b.toShort()
+
+    b = smallNum
+
+    isPunk = "false"
+
+    var k = y.toDouble()
+
+    b = k.toByte()
+
+    s = 0b10001
+
+    println(s)
+	
+}
