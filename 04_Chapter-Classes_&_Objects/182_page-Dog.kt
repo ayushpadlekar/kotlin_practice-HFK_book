526aafa6ffba0c44447af2339429b690173ce163
Ayush Padlekar <110286909+ayushpadlekar@users.noreply.github.com>
2025-03-14 23:57:41 +0530

    Update 182_page-Dog.kt

--- 04_Chapter-Classes_&_Objects/182_page-Dog.kt
+++ 04_Chapter-Classes_&_Objects/182_page-Dog.kt
@@ -1,17 +1,50 @@
+// HFK C4 page 182 - Dog class
 
-class Dog(val name: String, var weight: Int, val breed: String) {
+//Dog Class
+class Dog(val name: String, weight_param: Int, val breed: String) {
 
-    fun bark() {
-
-        println(if (weight < 20) "aau" else "bhau")
+    init {
+        println("Dog $name has been created.")
     }
 
+    var activities = arrayOf("Walks", "Runs", "Jumps")
+
+    var weight = weight_param
+    set(value) {
+        field = if (value > 0) value else 0
+    }
+
+    val weightInKgs: Double
+        get() = weight / 2.2
+
+    fun bark() {
+        println(if (weight < 20) "aau!" else "bhau!")
+    }
 }
 
+// Main function
 fun main() {
-	var myDog = Dog("Fido", 70, "Mixed")
+
+	var myDog = Dog("Fido", 15, "Mixed")
+
+    var friendsDog = Dog("Hudo", -1, "Golden Retreiver")
+    println()
 
     println(myDog.name)
-
     myDog.bark()
+    println("Weight: ${myDog.weight}")
+    println("Weight in kg: ${myDog.weightInKgs}")
+    myDog.activities = arrayOf("Walks", "Runs", "Jumps", "Catching Ball")
+    print("My dog enjoys: ")
+    for (item in myDog.activities) print("$item, ")
+
+    println(); println()
+
+    println(friendsDog.name)
+    friendsDog.bark()
+    println("Weight: ${friendsDog.weight}")
+    println("Weight in kg: ${friendsDog.weightInKgs}")
+    friendsDog.activities = arrayOf("Walks", "Runs", "Jumps", "Frisbee")
+    print("Friends dog enjoys: ")
+    for (item in friendsDog.activities) print("$item, ")
 }
