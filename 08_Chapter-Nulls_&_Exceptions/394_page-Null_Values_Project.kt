5a39c1e78865ab9131d584afefc4627081fd6545
Ayush Padlekar <110286909+ayushpadlekar@users.noreply.github.com>
2025-03-18 23:58:04 +0530

    Create 394_page-Null_Values_Project.kt

---
+++ 08_Chapter-Nulls_&_Exceptions/394_page-Null_Values_Project.kt
@@ -0,0 +1,58 @@
+// HFK C8 page 394 - Null Values Project
+
+class Wolf {
+
+    var hunger = 10
+    val food = "meat"
+
+    fun eat() {
+        println("The wolf is eating $food")
+    }
+}
+
+class myWolf {
+
+    var wolf: Wolf? = Wolf()
+
+    fun myFunction() {
+        wolf?.eat()
+    }
+}
+
+fun getAlphaWolf() : Wolf? {
+
+    return Wolf()
+}
+
+fun main() {
+	
+    var w: Wolf? = Wolf()
+
+    if (w != null) {
+        w.eat()
+    }
+
+    var x = w?.hunger
+    println("The value of x is $x")
+
+    var y = w?.hunger ?: -1
+    println("The value of y is $y")
+
+    var myWolf = myWolf()
+    myWolf?.wolf?.hunger = 8
+    println("The value of myWolf?.wolf?.hunger is ${myWolf?.wolf?.hunger}")
+    
+    var myArray = arrayOf("Hi", "Hello", null)
+    for (item in myArray) {
+        item?.let{
+            println(it)
+        }
+    }
+
+    getAlphaWolf()?.let {
+        it.eat()
+    }
+
+    w = null
+    var z = w!!.hunger
+}
