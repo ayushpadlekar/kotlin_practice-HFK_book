de92149df2db9bace28228d8d294cf12dd70a1f3
Ayush Padlekar <110286909+ayushpadlekar@users.noreply.github.com>
2025-03-19 03:38:25 +0530

    Update 394_page-Null_Values_Project.kt - added Comments

--- 08_Chapter-Nulls_&_Exceptions/394_page-Null_Values_Project.kt
+++ 08_Chapter-Nulls_&_Exceptions/394_page-Null_Values_Project.kt
@@ -1,5 +1,6 @@
 // HFK C8 page 394 - Null Values Project
 
+// Wolf Class
 class Wolf {
 
     var hunger = 10
@@ -10,6 +11,7 @@ class Wolf {
     }
 }
 
+// myWolf Class
 class myWolf {
 
     var wolf: Wolf? = Wolf()
@@ -19,29 +21,36 @@ class myWolf {
     }
 }
 
+// getAlphaWolf Function
 fun getAlphaWolf() : Wolf? {
-
     return Wolf()
 }
 
+// Main Function
 fun main() {
 	
-    var w: Wolf? = Wolf()
+    var w: Wolf? = Wolf() // nullable Wolf object in w variable
 
+    // if approach to check w is null or not
     if (w != null) {
         w.eat()
     }
 
+    // if w is not null, put value of hunger into x variable
     var x = w?.hunger
     println("The value of x is $x")
 
+    // Use of Elvis operator - if w & hunger is null use -1 value
+    // w = null
     var y = w?.hunger ?: -1
     println("The value of y is $y")
 
+    // myWolf object to access Wolf object properties
     var myWolf = myWolf()
     myWolf?.wolf?.hunger = 8
     println("The value of myWolf?.wolf?.hunger is ${myWolf?.wolf?.hunger}")
     
+    // This prints non null items of array - using 'let it'
     var myArray = arrayOf("Hi", "Hello", null)
     for (item in myArray) {
         item?.let{
@@ -49,10 +58,11 @@ fun main() {
         }
     }
 
+    // getAlphaWolf function call with 'let it' to access Wolf
     getAlphaWolf()?.let {
         it.eat()
     }
 
-    w = null
-    var z = w!!.hunger
+    // w = null
+    // var z = w!!.hunger
 }
