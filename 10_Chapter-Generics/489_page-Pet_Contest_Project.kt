c2c514caa31bf2d31b0f9fd783fffb6ddf6f9295
Ayush Padlekar <110286909+ayushpadlekar@users.noreply.github.com>
2025-03-24 02:49:41 +0530

    Update 489_page-Pet_Contest_Project.kt - Contest Updated

--- 10_Chapter-Generics/489_page-Pet_Contest_Project.kt
+++ 10_Chapter-Generics/489_page-Pet_Contest_Project.kt
@@ -1,53 +1,128 @@
 // HFK C10 Page 489 - Generic Pet, Contest, Retailers, Vet
 
 fun main() {
+
+    // Cat Objects
+    val catMancho = Cat("Mancho")
+    val catRancho = Cat("Rancho")
+    val catTancho = Cat("Tancho")
+    val catDancho = Cat("Dancho")
+    // Fish Objects
+    val fishFinny = Fish("Finny")
+    val fishMinny = Fish("Minny")
+    val fishKinny = Fish("Kinny")
+
+    // Cat Contest
+    println("--- Cat Contest -----------------------")
+    val catContest = Contest<Cat>()
+    catContest.addScore(catMancho, 30)
+    catContest.addScore(catRancho, 80)
+    catContest.addScore(catTancho, 60)
+    catContest.addScore(catDancho, 95)
+    println(catContest.scores)
+    val catWon = catContest.getWinners().first()
+    println("Winner: " + catWon.name)
+    println()
+
+    // Fish Contest
+    println("--- Fish Contest -----------------------")
+    val fishContest = Contest<Fish>()
+    fishContest.addScore(fishFinny, 50)
+    fishContest.addScore(fishMinny, 65)
+    fishContest.addScore(fishKinny, 85)
+    println(fishContest.scores)
+    val fishWon = fishContest.getWinners().first()
+    println("Winner: " + fishWon.name)
+    println()
+
+    // Pet Contest
+    println("--- Pet Contest -----------------------")
+    val petContest = Contest<Pet>()
+    petContest.addScore(fishFinny, 45)
+    petContest.addScore(fishMinny, 55)
+    petContest.addScore(catMancho, 35)
+    println(petContest.scores)
+    val petWon = petContest.getWinners().first()
+    println("Winner: " + petWon.name)
+    println()
+
+
 	
+
+    println(); println()
 }
 
 
-abstract class Pet {
+// Pet Superclass Abstract
+abstract class Pet(var name: String) {
 
 }
 
-class Cat: Pet {
+// Cat Sub-class
+class Cat(name: String): Pet(name) {
 
 }
 
-class Dog: Pet {
+// Dog Sub-class
+class Dog(name: String): Pet(name) {
 
 }
 
-class Fish: Pet {
-
-}
-
-
-class Contest {
+// Fish Sub-class
+class Fish(name: String): Pet(name) {
 
 }
 
 
 
-interface Retailer {
+// Contest class Generic
+class Contest<T: Pet> {
+
+    var scores: MutableMap<T, Int> = mutableMapOf()
+
+    fun addScore(t: T, scr: Int = 0) {
+
+        if(scr >= 0 && scr <= 100) scores.put(t, scr)
+    }
+
+    fun getWinners() : MutableSet<T> {
+
+        val highScore = scores.values.maxOrNull()
+        val winners: MutableSet<T> = mutableSetOf()
+
+        for((t, scr) in scores) {
+            if (scr == highScore) {
+                winners.add(t)
+            }
+        }
+        return winners
+    }
+}
+
+// Vet class
+class Vet<T> {
 
 }
 
-interface CatRetailer: Retailer {
+
+
+// Retailer interface Generic
+interface Retailer<T> {
 
 }
 
-interface DogRetailer: Retailer {
+// CatRetailer Sub-class
+class CatRetailer: Retailer<Cat> {
 
 }
 
-interface FishRetailer: Retailer {
+// DogRetailer Sub-class
+class DogRetailer: Retailer<Dog> {
+
+}
+
+// FishRetailer Sub-class
+class FishRetailer: Retailer<Fish> {
     
 }
-
-
-
-class Vet {
-
-}
-
 
