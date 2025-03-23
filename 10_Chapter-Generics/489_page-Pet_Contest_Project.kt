099415d37183cd059533447d7373079fbb8d8995
Ayush Padlekar <110286909+ayushpadlekar@users.noreply.github.com>
2025-03-23 23:19:07 +0530

    Create 489_page-Pet_Contest_Project.kt

---
+++ 10_Chapter-Generics/489_page-Pet_Contest_Project.kt
@@ -0,0 +1,53 @@
+// HFK C10 Page 489 - Generic Pet, Contest, Retailers, Vet
+
+fun main() {
+	
+}
+
+
+abstract class Pet {
+
+}
+
+class Cat: Pet {
+
+}
+
+class Dog: Pet {
+
+}
+
+class Fish: Pet {
+
+}
+
+
+class Contest {
+
+}
+
+
+
+interface Retailer {
+
+}
+
+interface CatRetailer: Retailer {
+
+}
+
+interface DogRetailer: Retailer {
+
+}
+
+interface FishRetailer: Retailer {
+    
+}
+
+
+
+class Vet {
+
+}
+
+
